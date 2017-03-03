b5d5a06b51ddf523603803a4eea14f7660a067e8
David Ayeke <ayekedavid@yahoo.com>
2017-03-03 16:56:46 -0600

    Added project scaffold

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 import os
+import sys
+import logging
+import time
+import json
+import getopt
 # Import SDK packages
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 
 
-def customCallback(*arg):
-    print("CustomCallback called with %d arguments: %s"%(len(arg),arg))
+# Custom Shadow callback
+def customShadowCallback_Update(payload, responseStatus, token):
+	# payload is a JSON string ready to be parsed using json.loads(...)
+	# in both Py2.x and Py3.x
+	if responseStatus == "timeout":
+		print("Update request " + token + " time out!")
+	if responseStatus == "accepted":
+		payloadDict = json.loads(payload)
+		print("~~~~~~~~~~~~~~~~~~~~~~~")
+		print("Update request with token: " + token + " accepted!")
+		print("property: " + str(payloadDict["state"]["desired"]["property"]))
+		print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+	if responseStatus == "rejected":
+		print("Update request " + token + " rejected!")
+
+def customShadowCallback_Delete(payload, responseStatus, token):
+	if responseStatus == "timeout":
+		print("Delete request " + token + " time out!")
+	if responseStatus == "accepted":
+		print("~~~~~~~~~~~~~~~~~~~~~~~")
+		print("Delete request with token: " + token + " accepted!")
+		print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+	if responseStatus == "rejected":
+		print("Delete request " + token + " rejected!")
 
 def main():
     # For certificate based connection
@@ -24,13 +51,19 @@ def main():
 
     myShadowClient.connect()
     # Create a device shadow instance using persistent subscription
-    myDeviceShadow = myShadowClient.createShadowHandlerWithName("Pi", True)
-    # Shadow operations
-    myDeviceShadow.shadowGet(customCallback, 5)
-    myDeviceShadow.shadowUpdate('{"test":true}', customCallback, 5)
-    myDeviceShadow.shadowDelete(customCallback, 5)
-    myDeviceShadow.shadowRegisterDeltaCallback(customCallback)
-    myDeviceShadow.shadowUnregisterDeltaCallback()
+    Bot = myShadowClient.createShadowHandlerWithName("Bot", True)
+
+    # Delete shadow JSON doc
+    Bot.shadowDelete(customShadowCallback_Delete, 5)
+
+    # Update shadow in a loop
+    loopCount = 0
+    # PUT ALL your code for handling buttons and such in here.
+    while True:
+    	JSONPayload = '{"state":{"desired":{"property":' + str(loopCount) + '}}}'
+    	Bot.shadowUpdate(JSONPayload, customShadowCallback_Update, 5)
+    	loopCount += 1
+    	time.sleep(1)
 
 
 if __name__ == '__main__':
