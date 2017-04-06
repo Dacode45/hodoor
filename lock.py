31876807a7ee14604bcb07c6228418e4bce6bc53
charlesahrensfeldman <charlesahrensfeldman@gmail.com>
2017-04-06 00:18:48 -0500

    gpio cleanup

--- lock.py
+++ lock.py
@@ -4,8 +4,10 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import time ## Import 'time' library. Allows us to use 'sleep'
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 21 to OUT
 
-GPIO.output(21,True)## Switch on pin 7
-time.sleep(100)## Wait
-GPIO.output(21,False)## Switch off pin 7
+GPIO.output(21,True)## Switch on pin 21
+time.sleep(0.1)## Wait
+GPIO.output(21,False)## Switch off pin 21
+
+GPIO.cleanup()
