77c7e5c07778768d574b17c1809d5b953073f04a
charlesahrensfeldman <charlesahrensfeldman@gmail.com>
2017-04-06 00:35:21 -0500

    I dunno, just try it

--- lock.py
+++ lock.py
@@ -6,8 +6,8 @@ import time ## Import 'time' library. Allows us to use 'sleep'
 GPIO.setmode(GPIO.BCM) ## Use board pin numbering
 GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 21 to OUT
 
-GPIO.output(21,True)## Switch on pin 21
-time.sleep(0.1)## Wait
-GPIO.output(21,False)## Switch off pin 21
+GPIO.output(21,1)## Switch on pin 21
+time.sleep(0.2)## Wait
+GPIO.output(21,0)## Switch off pin 21
 
 GPIO.cleanup()
