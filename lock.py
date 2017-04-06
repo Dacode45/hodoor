abd611cec4617be8696b17462dd477ae03261f29
charlesahrensfeldman <charlesahrensfeldman@gmail.com>
2017-04-06 00:20:37 -0500

    GPIO BCM

--- lock.py
+++ lock.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO ## Import GPIO library
 import time ## Import 'time' library. Allows us to use 'sleep'
 
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setmode(GPIO.BCM) ## Use board pin numbering
 GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 21 to OUT
 
 GPIO.output(21,True)## Switch on pin 21
