58b33d6b740854f7a70eecf2e3babe10cfbdf964
charlesahrensfeldman <charlesahrensfeldman@gmail.com>
2017-04-05 22:48:32 -0500

    changed pin to 21

--- lock.py
+++ lock.py
@@ -4,8 +4,8 @@ import RPi.GPIO as GPIO ## Import GPIO library
 import time ## Import 'time' library. Allows us to use 'sleep'
 
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
 
-GPIO.output(7,True)## Switch on pin 7
+GPIO.output(21,True)## Switch on pin 7
 time.sleep(100)## Wait
-GPIO.output(7,False)## Switch off pin 7
+GPIO.output(21,False)## Switch off pin 7
