ce7e5d538c8c61bd212e49bb3cd6148daff99dd0
David Ayeke <ayekedavid@yahoo.com>
2017-04-21 18:12:20 -0500

    Changes to setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-ssh -nNT -R 8081:localhost:8081 ubuntu@ec2-54-213-198-191.us-west-2.compute.amazonaws.com -i "/home/pi/.ssh/cse521.pem" &
-echo "ssh setup"
-source /home/pi/hodoor/pi.env
-echo "pi env setup"
-sudo iptables -I INPUT -p tcp --dport 8081 --syn -j ACCEPT
-echo "iptables setup"
-sudo -E node /var/www/html/index.js &
-echo "server running"
+
+case "$1" in
+start)
+  ssh -nNT -R 8081:localhost:8081 ubuntu@ec2-54-213-198-191.us-west-2.compute.amazonaws.com -i "/home/pi/.ssh/cse521.pem" &
+  iptables -I INPUT -p tcp --dport 8081 --syn -j ACCEPT
+  echo $1>/var/run/ssh.pid
+stop)
+  kill `cat /var/run/ssh.pid`
+  rm /var/run/ssh.pid
+  ;;
+restart)
+  $0 stop
+  $0 start
+  ;;
+status)
+  if [ -e /var/run/ssh.pid]; then
+    echo ssh tunnel is running, pid=`cat /var/run/ssh.pid`
+  else
+    echo ssh tunnel is not runnint
+    exit 1;
+  fi
+  ;;
+*)
+  echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
